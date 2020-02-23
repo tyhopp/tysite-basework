8ca5580dd7f556eea862a5f01b10d3a6e504b352
Ty Hopp <hopp.ty.c@gmail.com>
2020-02-23 17:05:02 +0800

    Fix header state logic

--- src/components/ty-header/ty-header.js
+++ src/components/ty-header/ty-header.js
@@ -2,23 +2,36 @@ import template from './ty-header.html';
 import './ty-header.css';
 
 class TyHeader extends HTMLElement {
+  constructor() {
+    super();
+    this._setCurrentNav = this._setCurrentNav.bind(this);
+  }
 
   connectedCallback() {
     if (!this._initialized) {
       this.appendChild(document.getTemplate(template));
       this._navs = this.querySelectorAll('.ty-header-nav');
       this._setCurrentNav();
+      this._setListeners(true);
       this._initialized = true;
     }
+  }
+
+  disconnectedCallback() {
+    this._setListeners(false);
   }
 
   _setCurrentNav() {
     const navs = this.querySelectorAll('.ty-header-nav');
     Array.from(navs).forEach(nav => {
-      if (nav.href === window.location.href) {
-        nav.classList.add('ty-header-nav--current');
-      }
-    })
+      const isCurrentRoute = nav.href === window.location.href;
+      nav.classList[isCurrentRoute ? 'add' : 'remove']('ty-header-nav--current');
+    });
+  }
+
+  _setListeners(flag) {
+    const fnName = flag ? 'addEventListener' : 'removeEventListener';
+    window[fnName]('popstate', this._setCurrentNav);
   }
 }
 
--- src/pages/base.js
+++ src/pages/base.js
@@ -2,7 +2,6 @@
 import 'utils/template-util.js';
 import 'utils/skeleton-util.js';
 import 'utils/router-util.js';
-// import 'utils/theme-util.js';
 
 // App styles
 import 'styles/main.css';
